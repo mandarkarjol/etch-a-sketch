38ede22313c3a8daf556bd72f8684e457640870a
mandarkarjol <mandar.karjol@gmail.com>
2025-03-29 08:30:42 -0700

    Updates makeSquareGrid logic
    Changes logic to keep canvas size fixed and make
    size of squares inside the grid to be flexible.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Define a function that generates a square grid inside a target html div element. Default grid size = 16
-function makeSquareGrid(targetDiv, size = 40){
+function makeSquareGrid(targetDiv, size = 16){
     for(let i = 0; i <= (size * size) - 1; i++ ){
         const squareDiv = document.createElement("div");
+        squareDiv.style.width = `${targetDiv.clientWidth / size}px`;
         targetDiv.append(squareDiv);
-        console.log(`Box ${i} created.`);
     };
-    targetDiv.style.width = `${20 * size}px`;
+    
 };
 const containerDiv = document.querySelector(".container");
 makeSquareGrid(containerDiv);
